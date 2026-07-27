211c2a591292b0a4acb3dc7e57b6d5473890b024
Xeno <xenogorillatag@gmail.com>
2026-07-26 21:52:01 -0400

    Code + Proj Files

---
+++ NightNoRain/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+
+
+namespace NightNoRain
+{
+    [BepInPlugin("com.xeno.gorillatag.nightnorain", "NightNoRain", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Update()
+        {
+            for (int i = 1; i < BetterDayNightManager.instance.weatherCycle.Length; i++)
+            {
+                BetterDayNightManager.instance.weatherCycle[i] = BetterDayNightManager.WeatherType.None;
+            }
+            BetterDayNightManager.instance.SetTimeOfDay(8);
+        }
+
+    }
+}
